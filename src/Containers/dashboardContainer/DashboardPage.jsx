d8c1dcc7e0c17f06b251f99f4809cb0525a8ab46
Olivia Auzenne <oauzenne@gmail.com>
2023-11-22 22:22:25 -0600

    starting a new project

---
+++ src/Containers/dashboardContainer/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardPage = () => {
+    return (
+      
+        <div>
+            <h3>Hello</h3>
+        </div>
+    );
+};
+
+export default DashboardPage;
---
+++ src/Containers/dashboardContainer/index.jsx
@@ -0,0 +1,3 @@
+import DashboardPage from './DashboardPage.jsx';
+
+export default DashboardPage;
